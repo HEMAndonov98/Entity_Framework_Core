d165f2fee460ca77f4b9bbd00a6d96a86325bee7
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-02-28 23:15:27 +0200

    Added Persist method

--- ORM Fundamental Exercise/MiniORM/DbContext.cs
+++ ORM Fundamental Exercise/MiniORM/DbContext.cs
@@ -21,5 +21,82 @@ namespace MiniORM
             typeof(bool),
             typeof(DateTime),
         };
+
+        protected DbContext(string connectionString)
+        {
+            this._connection = new DatabaseConnection(connectionString);
+            this._dbSetProperties = this.DiscoverDbSets();
+
+            using (new ConnectionManager(this._connection))
+            {
+                this.InitializeDbSets();
+            }
+            this.MapAllRelations();
+        }
+
+        public async Task SaveChanges()
+        {
+            var dbSets = _dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                var invalidEntities = dbSet.Where(entity => !this.IsObjectValid(entity)).ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException(string.Format(ExceptionMessages.NonExistingEntitiesExceptionMessage, invalidEntities.Length, dbSet.GetType().Name));
+                }
+            }
+
+            using (new ConnectionManager(this._connection))
+            {
+                using (var transaction = this._connection.StartTransaction())
+                {
+                    foreach (IEnumerable<object> dbSet in dbSets)
+                    {
+                        var dbSetType = dbSet.GetType().GetGenericArguments().First();
+                        var persistMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic)
+                            .MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+                        catch (TargetInvocationException tie)
+                        {
+                            throw tie.InnerException;
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            await transaction.RollbackAsync();
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsObjectValid(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IDictionary<Type, PropertyInfo>? DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ORM Fundamental Exercise/MiniORM/ExceptionMessages.cs
+++ ORM Fundamental Exercise/MiniORM/ExceptionMessages.cs
@@ -4,6 +4,7 @@ namespace MiniORM
 	public static class ExceptionMessages
 	{
 		public static string NullEntityExceptionMessage = @"Item cannot be null";
+		public static string NonExistingEntitiesExceptionMessage = @"{0} Invalid Entities found in {1}!";
     }
 }
 
