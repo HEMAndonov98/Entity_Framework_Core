c8d6650d69e183279b7c611f0ee587ac1c9371df
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-03-29 21:54:03 +0300

    Migrations

---
+++ Auto Mapper Exercise/FastFood.Data/Data/Migrations/20230329144246_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastFood.Data.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(max)",
+                precision: 3,
+                scale: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldPrecision: 3,
+                oldScale: 30);
+        }
+    }
+}
