6a2b524b252e8826f12c0d61aabbfb0c1584438d
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-04-26 23:01:26 +0300

    Migrations and Results

---
+++ XML Processing Exercises/ProductShop/Migrations/20230426183417_FixedProductSellerIdBeingSetToBuyerId.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductShop.Migrations
+{
+    public partial class FixedProductSellerIdBeingSetToBuyerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_BuyerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_BuyerId",
+                table: "Products",
+                column: "BuyerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_BuyerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_BuyerId",
+                table: "Products",
+                column: "BuyerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
