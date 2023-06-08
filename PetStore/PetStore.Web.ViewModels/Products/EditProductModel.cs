688e9defe1a0b2b69edc7c33afe374543727926c
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-06-09 01:41:06 +0300

    Added Validation Constraints to Product Edit Model

--- PetStore/PetStore.Web.ViewModels/Products/EditProductModel.cs
+++ PetStore/PetStore.Web.ViewModels/Products/EditProductModel.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using PetStore.Common;
+
 namespace PetStore.Web.ViewModels.Products;
 
 public class EditProductModel
 {
     public string Id { get; set; } = null!;
 
+    [Required]
+    [StringLength(ProductInputValidationConstants.NameMaxLength,
+        MinimumLength = ProductInputValidationConstants.NameMinLength,
+        ErrorMessage = ProductInputValidationConstants.InvalidNameLengthErrorMessage)]
     public string Name { get; set; } = null!;
 
+    [Required]
     public decimal Price { get; set; }
-
+    
+    [Required]
     public int CategoryId { get; set; }
 }
