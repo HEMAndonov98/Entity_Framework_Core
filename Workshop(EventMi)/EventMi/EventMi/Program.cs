ed1d2779fae388476fd9695fd1492fa27ede006f
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-06-24 17:07:59 +0300

    Added DbContext to app

--- Workshop(EventMi)/EventMi/EventMi/Program.cs
+++ Workshop(EventMi)/EventMi/EventMi/Program.cs
@@ -1,7 +1,13 @@
+using EventMi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<EventMiDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EventMiConnection")));
 
 var app = builder.Build();
 
