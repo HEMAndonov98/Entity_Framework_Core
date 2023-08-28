4a501f025d36e5cd065633606d6c7161435c0e90
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-08-28 20:53:43 +0300

    Migrations

---
+++ Workshop(Blog)/Data/Blog.Data/Migrations/20230828105905_FiedArticleTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreTemplate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FiedArticleTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Articles",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Articles",
+                newName: "Type");
+        }
+    }
+}
